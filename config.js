cc022fbd3bf2a3599c27ca0bb03a48f98d5d8d90
nexusthede <ivanrrr76@gmail.com>
2026-02-19 18:02:08 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  TOKEN: process.env.BOT_TOKEN, // required, bot token
-  PREFIX: process.env.BOT_PREFIX || ".", // defaults to "."
-  JOIN_TO_CREATE_ID: process.env.JOIN_VC_CHANNEL || "1474219896190668892",
-  CATEGORY_ID: process.env.VC_CATEGORY || "1474219814305402930",
-  CHAT_LB_CHANNEL: process.env.CHAT_LB_CHANNEL || null, // optional, can set via .set
-  VC_LB_CHANNEL: process.env.VC_LB_CHANNEL || null,     // optional, can set via .set
-  ALLOWED_GUILD: process.env.ALLOWED_GUILD || "1449708401050259457", // your server only
-  PORT: process.env.PORT || 3000 // for Render/BetterStack health ping
+  TOKEN: process.env.BOT_TOKEN,                // required
+  PREFIX: ".",                                 // default prefix
+  JOIN_TO_CREATE_ID: "1474219896190668892",    // default join-to-create VC
+  CATEGORY_ID: "1474219814305402930",         // default VC category
+  CHAT_LB_CHANNEL: null,                       // set later with .set
+  VC_LB_CHANNEL: null,                         // set later with .set
+  ALLOWED_GUILD: "1449708401050259457",       // your server only
+  PORT: 3000                                   // default port for BetterStack
 };
