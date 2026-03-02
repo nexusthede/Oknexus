a683914fa0e0e260482f36a0c291577b4c490475
nexusthede <ivanrrr76@gmail.com>
2026-03-02 01:04:24 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  TOKEN: process.env.BOT_TOKEN,
-  PREFIX: ".",
-  JOIN_TO_CREATE_ID: "1474219896190668892",
-  CATEGORY_ID: "1474219814305402930",
-  CHAT_LB_CHANNEL: null,
-  VC_LB_CHANNEL: null,
-  ALLOWED_GUILD: "1449708401050259457",
-  PORT: 3000
+  TOKEN: process.env.BOT_TOKEN,                 // Bot token from Render secrets
+  PREFIX: ".",                                   // Command prefix
+  JOIN_TO_CREATE_ID: "1477555779933573233",     // Join-to-create VC channel ID
+  CATEGORY_ID: "1477554377387675651",          // Category ID where temp VCs will be created
+  CHAT_LB_CHANNEL: null,                        // Optional: chat leaderboard channel ID
+  VC_LB_CHANNEL: null,                          // Optional: VC leaderboard channel ID
+  ALLOWED_GUILD: "1449708401050259457",        // Your server ID
+  PORT: 3000                                    // Default port for hosting
 };
